6591b25ea71a9f4947cb341ba34b8294559e321b
xaparge5 <84373344+xaparge5@users.noreply.github.com>
2025-07-27 18:40:58 +0300

    efcore init

---
+++ Application/Test/TestAppService.cs
@@ -0,0 +1,34 @@
+﻿using Application.Contracts.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EFCore.DBContext;
+namespace Application.Test
+{
+    public class TestAppService : ITestAppService
+    {
+        private readonly AppDbContext _dbcontext;
+
+        public async Task<bool> CreateAsync(string name)
+        {
+            try
+            {
+                var test = new Domain.Test.Test
+                {
+                    Id = Guid.NewGuid(),
+                    Name = name,
+                    CreationTime = DateTime.Now
+                };
+                await _dbcontext.Tests.AddAsync(test);
+                await _dbcontext.SaveChangesAsync();
+                return true;
+            }
+            catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
--- Domain/obj/Debug/net7.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net7.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b277b3c6b916612f2003913630e3af742f4653c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0cf08539ecee3717626d8864dd4f843461c4cb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
