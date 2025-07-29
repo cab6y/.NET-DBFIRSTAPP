5ea692142a12b87a3f496f1b75578feb51a7f32c
xaparge5 <84373344+xaparge5@users.noreply.github.com>
2025-07-29 23:57:39 +0300

    remove view folder

--- Application/obj/Debug/net7.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net7.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4227d748e8f941a37e1305a556024afa60b81fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cf5d0fcafe677af3424f29ec58e416fa20a0bca")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
